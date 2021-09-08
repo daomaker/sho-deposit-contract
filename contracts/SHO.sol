cf44ccfbd39b20512333a8e9ccaf598f27eabd5a
busypeter <busypeter@live.com>
2021-09-08 14:46:13 +0200

    Minor refactoring to improve readability and clarity of error messages (#1)
    * Minor refactoring to improve readability and clarity of error messages
    
    * Update .gitignore
    
    * Address CR feedback and update tests

--- contracts/SHO.sol
+++ contracts/SHO.sol
@@ -1,62 +1,68 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SHO is Ownable {
     using ECDSA for bytes32;
 
-    IERC20 public depositToken;
+    address public shoOrganizer;
     address public depositReceiver;
-    address public organizer;
+    IERC20 public depositToken;
 
-    mapping(uint => mapping(address => bool)) public deposits;
+    mapping(uint => mapping(address => bool)) public depositsForSho;
 
+    event ShoOrganizerChanged(
+        address oldShoOrganizer,
+        address newShoOrganizer
+    );
+
+    event DepositReceiverChanged(
+        address oldDepositReceiver,
+        address newDepositReceiver
+    );
+
+    event DepositTokenChanged(
+        IERC20 oldDepositToken,
+        IERC20 newDepositToken
+    );
+    
     event Deposited(
         address winner,
         uint indexed shoId,
         uint amount,
         uint deadline,
         address depositReceiver,
-        address organizer
-    );
-    
-    event DepositTokenChanged(
-        IERC20 oldDepositToken,
-        IERC20 newDepositToken
-    );
-    
-    event DepositReceiverChanged(
-        address oldDepositReceiver,
-        address newDepositReceiver
+        address shoOrganizer
     );
 
-    event OrganierChanged(
-        address oldOrganier,
-        address newOrganizer
-    );
-
-    constructor(IERC20 _depositToken, address _depositReceiver, address _organizer) {
-        depositToken = _depositToken;
+    constructor( address _shoOrganizer, address _depositReceiver, IERC20 _depositToken) {
+        shoOrganizer = _shoOrganizer;
         depositReceiver = _depositReceiver;
-        organizer = _organizer;
+        depositToken = _depositToken;
+
+        emit ShoOrganizerChanged(address(0), _shoOrganizer);
+        emit DepositReceiverChanged(address(0), _depositReceiver);
+        emit DepositTokenChanged(IERC20(address(0)), _depositToken);
+    }
+
+    function setShoOrganizer(address _shoOrganizer) external onlyOwner {
+        shoOrganizer = _shoOrganizer;
+
+        emit ShoOrganizerChanged(shoOrganizer, _shoOrganizer);
+    }
+    function setDepositReceiver(address _depositReceiver) external onlyOwner {
+        depositReceiver = _depositReceiver;
+
+        emit DepositReceiverChanged(depositReceiver, _depositReceiver);
     }
 
     function setDepositToken(IERC20 _depositToken) external onlyOwner {
-        emit DepositTokenChanged(depositToken, _depositToken);
         depositToken = _depositToken;
-    }
 
-    function setDepositReceiver(address _depositReceiver) external onlyOwner {
-        emit DepositReceiverChanged(depositReceiver, _depositReceiver);
-        depositReceiver = _depositReceiver;
-    }
-
-    function setOrganizer(address _organizer) external onlyOwner {
-        emit OrganierChanged(organizer, _organizer);
-        organizer = _organizer;
+        emit DepositTokenChanged(depositToken, _depositToken);
     }
 
     function deposit(
@@ -67,16 +73,17 @@ contract SHO is Ownable {
         address _depositReceiver
     ) external {
         address winner = msg.sender;
-        require(_depositReceiver == depositReceiver, "SHO: wrong deposit receiver");
-        require(!deposits[shoId][winner], "SHO: deposited");
-        require(block.timestamp <= deadline, "SHO: deadline");
+
+        require(_depositReceiver == depositReceiver, "SHO: invalid deposit receiver");
+        require(!depositsForSho[shoId][winner], "SHO: this wallet already made a deposit for this SHO");
+        require(block.timestamp <= deadline, "SHO: the deadline for this SHO has passed");
 
         bytes32 dataHash = keccak256(abi.encodePacked(winner, shoId, amount, deadline, _depositReceiver));
-        require(dataHash.toEthSignedMessageHash().recover(signature) == organizer, "SHO: signature verification failed");
+        require(dataHash.toEthSignedMessageHash().recover(signature) == shoOrganizer, "SHO: signature verification failed");
 
-        deposits[shoId][winner] = true;
+        depositsForSho[shoId][winner] = true;
         depositToken.transferFrom(winner, _depositReceiver, amount);
 
-        emit Deposited(winner, shoId, amount, deadline, _depositReceiver, organizer);
+        emit Deposited(winner, shoId, amount, deadline, _depositReceiver, shoOrganizer);
     }
 }

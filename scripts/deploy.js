7176524fed058527813a8f1533252423cf95107b
ihux3 <ihux3x@gmail.com>
2021-09-14 11:03:31 +0200

    removed depositToken from deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,9 @@
 async function main() {
     const organizer = "0x304E7ADdF70A4B0EA27C46483D43cc27e40c35De";
-    const depositReceiver = "0xcF28556EE95Be8c52AD2f3480149128cCA51daC1";
-    const depositToken = "0x5929f5e0c9522f2decd9ac8dfa6cf60b5188bb38";
+    const depositReceiver = "0xcF28556EE95Be8c52AD2f3480149128cCA51daC1"
 
     const SHO = await ethers.getContractFactory("SHO");
-    const sho = await SHO.deploy(organizer, depositReceiver, depositToken);
+    const sho = await SHO.deploy(organizer, depositReceiver);
     await sho.deployed();
 
     console.log("SHO deployed to:", sho.address);
